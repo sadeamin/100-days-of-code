da125170f9c9a9d78e108af66cc3c150d2e5c13d
Eamin <sadeamin13@gmail.com>
2021-06-20 23:59:56 +0600

    complete all work from day 24

--- Intermediate/day_24/main.py
+++ Intermediate/day_24/main.py
@@ -6,8 +6,12 @@
 #     file.write("\nNew text.")
 
 
-with open("my_file.txt", mode="a") as file:
-    file.write("\nnew text")
+# with open("C:/Users/Eamin/Desktop/my_file.txt", mode="a") as file:
+#     file.write("\nMy brother's name is alamin khan.")
 
 # with open("new_file.text", mode="w") as file:
 #     file.write("\nHi.")
+
+with open("/Users/Eamin/Desktop/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
